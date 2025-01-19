5c27fe3a472d304b8254e3434a70282a564729fc
Cheyi Wu <wu.chey@northeastern.edu>
2025-01-18 21:51:10 -0500

    Add missing stuff

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -16,7 +16,9 @@ export default function Modules() {
       <button>+Module</button>
       <ul id="wd-modules">
         <li className="wd-module">
-          <div className="wd-title">Week 1</div>
+          <div className="wd-title">
+            Week 1, Lecture 1- CourseIntroduction, Syllabus, Agenda
+          </div>
           <ul className="wd-lessons">
             <li className="wd-lesson">
               <span className="wd-title">LEARNING OBJECTIVES</span>
@@ -25,6 +27,47 @@ export default function Modules() {
                 <li className="wd-content-item">
                   Learn what is Web Development
                 </li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">READING</span>
+              <ul>
+                <li>Full Stack Developer-Chapter 1 - Introduction</li>
+                <li>Full Stack Developer - Chapter 2 - Creating Us</li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul>
+                <li>Introduction to Web Development</li>
+                <li>Creating an HTTP server with Node.js</li>
+                <li>Creating a React Application</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li className="wd-module">
+          <div className="wd-title">
+            Week 1, Lecture 2- Formattring User Interfaces with HTML
+          </div>
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Learn how to create user interfaces with HTML
+                </li>
+                <li className="wd-content-item">
+                  Deploy the assignment to Netflify
+                </li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul>
+                <li>Introduction to HTML and the DOM</li>
+                <li>Formatting Web content with Headings and</li>
+                <li>Formatting content with Lists and Tables</li>
               </ul>
             </li>
           </ul>
--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -20,8 +20,96 @@ export default function Dashboard() {
             </div>
           </Link>
         </div>
-        <div className="wd-dashboard-course"> ... </div>
-        <div className="wd-dashboard-course"> ... </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/2345/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/src/assets/java.png" width={200} />
+            <div>
+              <h5> CS2345 java </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/2000/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/src/assets/angular.png" width={200} />
+            <div>
+              <h5> CS2000 Angular </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/1500/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/src/assets/python.jpg" width={200} />
+            <div>
+              <h5> CS1500 Python </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/2200/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/src/assets/c++.png" width={200} />
+            <div>
+              <h5> CS 2200 C++ </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/2300/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/src/assets/vue.png" width={200} />
+            <div>
+              <h5> CS 2300 Vue.js </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/2300/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/src/assets/csharp.jpg" width={200} />
+            <div>
+              <h5> CS 2300 C# </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
