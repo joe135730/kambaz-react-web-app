c3e45ba54c2d41a5c05a062a74e9faa9fac8a127
Cheyi Wu <wu.chey@northeastern.edu>
2025-01-19 16:58:20 -0500

    Add GitHub Link

--- src/Labs/landing.tsx
+++ src/Labs/landing.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router";
 export default function Labs() {
     return (
       <div id="wd-labs">
         <h1>Name: Che-Yi Wu</h1>
         <h2>Section: 01</h2>
         <h2>CRN: 36549 | Online</h2>
+        <h2><a href="https://github.com/joe135730/kambaz-react-web-app" id="wd-github-link" target="_blank">Github Link</a></h2>
       </div>
     );
   }
